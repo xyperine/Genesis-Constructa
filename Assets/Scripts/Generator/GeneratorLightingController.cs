4b23c5e09e4d695180698d0bfc861552cc4cda3b
ewsannne <ewsannne@gmail.com>
2022-06-02 20:04:16 +1000

    Removed lights from Generator

--- Assets/Scripts/Generator/GeneratorLightingController.cs
+++ Assets/Scripts/Generator/GeneratorLightingController.cs
@@ -1,36 +1,30 @@
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace MoonPioneerClone.Generator
 {
     public sealed class GeneratorLightingController : MonoBehaviour
     {
-        [SerializeField] private Light coreLight;
         [SerializeField] private MeshRenderer meshRenderer;
-
         [SerializeField] private float lightLevel = 1f;
 
         private static readonly int EmissionStrengthPropertyID = Shader.PropertyToID("_Emission_Strength");
 
         private Material _material;
         
-        private float _defaultLightIntensity;
-        private float _defaultMaterialEmission;
+        private float _defaultEmission;
 
 
         private void Awake()
         {
             _material = meshRenderer.material;
             
-            _defaultLightIntensity = coreLight.intensity;
-            _defaultMaterialEmission = _material.GetFloat(EmissionStrengthPropertyID);
+            _defaultEmission = _material.GetFloat(EmissionStrengthPropertyID);
         }
 
 
         private void Update()
         {
-            coreLight.intensity = _defaultLightIntensity * lightLevel;
-            _material.SetFloat(EmissionStrengthPropertyID, _defaultMaterialEmission * lightLevel);
+            _material.SetFloat(EmissionStrengthPropertyID, _defaultEmission * lightLevel);
         }
     }
 }
