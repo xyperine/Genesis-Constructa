e2fe6525c1b7289fa92d7e58ae6f040787b0c7a9
ewsannne <ewsannne@gmail.com>
2022-06-14 23:40:06 +1000

    Multiple key items functionality for one extractor

---
+++ Assets/Scripts/ItemsExtraction/Extra/KeyItemSystem/KeyItemSlot.cs
@@ -0,0 +1,26 @@
+﻿using ColonizationMobileGame.ItemsPlacementsInteractions;
+using ColonizationMobileGame.ItemsPlacementsInteractions.StackZoneLogic;
+
+namespace ColonizationMobileGame.ItemsExtraction.Extra.KeyItemSystem
+{
+    public class KeyItemSlot : StackZone
+    {
+        private KeyItem _item;
+        
+        public bool Satisfied => HasItems;
+
+
+        public override void Add(StackZoneItem item)
+        {
+            base.Add(item);
+
+            _item = item.GetComponent<KeyItem>();
+        }
+
+
+        public void Tick()
+        {
+            _item.Tick();
+        }
+    }
+}
---
+++ Assets/Scripts/ItemsExtraction/Extra/KeyItemSystem/KeyItemsSet.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace ColonizationMobileGame.ItemsExtraction.Extra.KeyItemSystem
+{
+    public class KeyItemsSet : MonoBehaviour
+    {
+        [SerializeField] private KeyItemSlot[] slots;
+        
+        public bool Satisfied => slots.All(s => s.Satisfied);
+
+
+        private void Update()
+        {
+            if (!Satisfied)
+            {
+                return;
+            }
+            
+            UpdateEverySlot();
+        }
+
+
+        private void UpdateEverySlot()
+        {
+            foreach (KeyItemSlot slot in slots)
+            {
+                slot.Tick();
+            }
+        }
+    }
+}
