19ad6891dce156965ef49f698190b7a0784c69ce
ewsannne <ewsannne@gmail.com>
2022-08-26 02:14:52 +1000

    Added options in menu toolbar to clear or open save file

---
+++ Assets/Scripts/Editor/SaveLoadSystem/SaveFile.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.IO;
+using ColonizationMobileGame.SaveLoadSystem;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+
+namespace ColonizationMobileGame.Editor.SaveLoadSystem
+{
+    public static class SaveFile
+    {
+        private static string FullPath => SaveSystemSettings.instance.FullPath;
+        
+        
+        [MenuItem("Saving/Save File/Open")]
+        public static void Open()
+        {
+            if (!File.Exists(FullPath))
+            {
+                Debug.LogWarning($"{FullPath} was not found!");
+                return;
+            }
+            
+            Process.Start(FullPath);
+        }
+        
+        
+        [MenuItem("Saving/Save File/Clear")]
+        public static void Clear()
+        {
+            File.Delete(FullPath);
+        }
+    }
+}
