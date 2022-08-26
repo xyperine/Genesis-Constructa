d4f4299e7055ace66173c46b4c388f56fbdd944f
ewsannne <ewsannne@gmail.com>
2022-08-26 23:43:37 +1000

    Saving player's position

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,24 @@
+using System;
+using ColonizationMobileGame.SaveLoadSystem;
 using UnityEngine;
 
 namespace ColonizationMobileGame.Player
 {
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : MonoBehaviour, ISaveable
     {
         [SerializeField] private Joystick joystick;
         [SerializeField] private float speed;
         [SerializeField, Range(0f, 10f)] private float smoothness;
+
+        [SerializeField, HideInInspector] private PermanentGuid guid;
 
         private const float SMOOTHNESS_MODIFIER = 1f / 8f;
         
         private Vector3 _velocity;
 
         public float RelativeVelocity => Mathf.Clamp01(_velocity.magnitude / Time.fixedDeltaTime / speed);
+
+        public PermanentGuid Guid => guid;
 
 
         private void Update()
@@ -47,6 +53,30 @@ namespace ColonizationMobileGame.Player
             Vector3 newPosition = transform.position + _velocity;
             Quaternion newRotation = Quaternion.LookRotation(-_velocity);
             transform.SetPositionAndRotation(newPosition, newRotation);
+        }
+
+
+        public object Save()
+        {
+            return new SaveData
+            {
+                Position = transform.position,
+            };
+        }
+
+
+        public void Load(object data)
+        {
+            SaveData saveData = (SaveData) data;
+
+            transform.position = saveData.Position;
+        }
+        
+        
+        [Serializable]
+        private struct SaveData
+        {
+            public Vector3 Position { get; set; }
         }
     }
 }
