880722f1fc7f2677f923e9c9e77ba471faab1cb3
ewsannne <ewsannne@gmail.com>
2022-08-28 13:48:20 +1000

    Saving progression milestones (temporary like this)

--- Assets/Scripts/ProgressionMilestonesSystem/ProgressionMilestone.cs
+++ Assets/Scripts/ProgressionMilestonesSystem/ProgressionMilestone.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using ColonizationMobileGame.SaveLoadSystem;
 using UnityEngine;
 
 namespace ColonizationMobileGame.ProgressionMilestonesSystem
 {
-    public abstract class ProgressionMilestone : MonoBehaviour, IProgressionMilestone
+    public abstract class ProgressionMilestone : MonoBehaviour, IProgressionMilestone, ISaveableWithGuid
     {
+        [SerializeField, HideInInspector] private PermanentGuid guid;
+        
         protected abstract ProgressionMilestoneType Type { get; }
+
+        public bool IsAchieved { get; private set; }
+        public PermanentGuid Guid => guid;
         
         public event Action<ProgressionMilestoneType> Achieved;
 
@@ -31,8 +37,37 @@ namespace ColonizationMobileGame.ProgressionMilestonesSystem
         protected void InvokeAchieved()
         {
             Achieved?.Invoke(Type);
+            IsAchieved = true;
 
             gameObject.SetActive(false);
+        }
+
+
+        public object Save()
+        {
+            return new SaveData
+            {
+                IsAchieved = IsAchieved,
+            };
+        }
+
+
+        public void Load(object data)
+        {
+            SaveData saveData = (SaveData) data;
+
+            IsAchieved = saveData.IsAchieved;
+            if (IsAchieved)
+            {
+                InvokeAchieved();
+            }
+        }
+        
+        
+        [Serializable]
+        private struct SaveData
+        {
+            public bool IsAchieved { get; set; }
         }
     }
 }
