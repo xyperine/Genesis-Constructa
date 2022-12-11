f7581018882798e49d7e00abcb0d24baa5d38984
ewsannne <ewsannne@gmail.com>
2022-12-11 10:43:02 +0400

    Created TimeScaleController script for better debugging

---
+++ Assets/Scripts/TimeScaleController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ColonizationMobileGame
+{
+    public class TimeScaleController : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 1f)] private float timeScale;
+        
+        
+        private void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                Time.timeScale = timeScale;
+            }
+        }
+    }
+}
