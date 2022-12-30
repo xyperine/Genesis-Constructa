12091d8a1b8f5ac6838ce65d515cbf039c82c234
ewsannne <ewsannne@gmail.com>
2022-12-30 06:59:12 +0400

    Added option for FramerateSetter to work in the Editor

--- Assets/Scripts/Initialization/FramerateSetter.cs
+++ Assets/Scripts/Initialization/FramerateSetter.cs
@@ -4,14 +4,27 @@ namespace ColonizationMobileGame.Initialization
 {
     public class FramerateSetter : MonoBehaviour
     {
+#if UNITY_EDITOR
+        [SerializeField] private bool workInEditor;
+#endif
         [SerializeField, Range(0, 60)] private int targetFramerate = 60;
         
         
-#if !UNITY_EDITOR
         private void Awake()
         {
+            SetFramerate();
+        }
+
+
+        private void SetFramerate()
+        {
+#if UNITY_EDITOR
+            if (!workInEditor)
+            {
+                return;
+            }
+#endif
             Application.targetFrameRate = targetFramerate;
         }
-#endif
     }
 }
