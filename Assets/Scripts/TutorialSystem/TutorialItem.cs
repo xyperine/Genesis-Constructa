3836e9537b7ebe2d4de90b6d9e7c332db7e99e0a
ewsannne <ewsannne@gmail.com>
2023-02-27 09:23:56 +0400

    Implemented saving for arrow pointers

--- Assets/Scripts/TutorialSystem/TutorialItem.cs
+++ Assets/Scripts/TutorialSystem/TutorialItem.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using ColonizationMobileGame.SaveLoadSystem;
 using ColonizationMobileGame.UI.ArrowPointers.Target;
 using UnityEngine;
 
 namespace ColonizationMobileGame.TutorialSystem
 {
-    public class TutorialItem : MonoBehaviour, IArrowPointerTargetProvider
+    public class TutorialItem : MonoBehaviour, IArrowPointerTargetProvider, IPermanentGuidIdentifiable
     {
         [SerializeField] private TutorialStep step;
+
+        [SerializeField, HideInInspector] private PermanentGuid guid;
         
         public TutorialStep Step => step;
-        
+        public PermanentGuid Guid => guid;
+
         public event Action<Transform> TargetReady;
 
 
--- Assets/Scripts/UI/ArrowPointers/Target/ArrowPointersTargetsManager.cs
+++ Assets/Scripts/UI/ArrowPointers/Target/ArrowPointersTargetsManager.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using ColonizationMobileGame.SaveLoadSystem;
+using ColonizationMobileGame.Utility.Extensions;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace ColonizationMobileGame.UI.ArrowPointers.Target
 {
-    public class ArrowPointersTargetsManager : MonoBehaviour
+    public class ArrowPointersTargetsManager : MonoBehaviour, ISceneSaveable
     {
         [SerializeField] private ArrowPointersManager pointersManager;
         
+        [SerializeField, HideInInspector] private PermanentGuid guid;
+
         private readonly List<ArrowPointerTarget> _targets = new List<ArrowPointerTarget>();
         
         private Camera _camera;
 
         private TargetsFactory _targetsFactory;
         private IArrowPointerTargetProvider[] _targetProviders;
+
+        private List<string> _guids = new List<string>();
+        
+        public PermanentGuid Guid => guid;
+        public int LoadingOrder => -2;
 
 
         public void SetFactory(TargetsFactory targetsFactory)
@@ -45,7 +55,26 @@ namespace ColonizationMobileGame.UI.ArrowPointers.Target
 
         private void AddTarget(Transform targetTransform)
         {
+            if (IsAlreadyShown(targetTransform))
+            {
+                return;
+            }
+            
             AddTargetWhenReady(targetTransform).Forget();
+        }
+
+
+        private bool IsAlreadyShown(Transform targetTransform)
+        {
+            IPermanentGuidIdentifiable identifiable = targetTransform.GetComponent<IPermanentGuidIdentifiable>();
+
+            if (identifiable == null)
+            {
+                throw new InvalidDataException(
+                    $"{targetTransform.gameObject.GetFullName()} has no identifiable component!");
+            }
+
+            return _guids.Contains(identifiable.Guid.Value);
         }
 
 
@@ -60,13 +89,16 @@ namespace ColonizationMobileGame.UI.ArrowPointers.Target
             {
                 return;
             }
-
+            
             ArrowPointerTarget target = _targetsFactory!.GetTarget(targetTransform);
 
             _targets.Add(target);
             pointersManager.PointTo(target);
 
             target.Invalidated += RemoveTarget;
+
+            IPermanentGuidIdentifiable identifiable = targetTransform.GetComponent<IPermanentGuidIdentifiable>();
+            _guids.Add(identifiable?.Guid.Value);
         }
 
 
@@ -105,6 +137,29 @@ namespace ColonizationMobileGame.UI.ArrowPointers.Target
             bool onScreen = viewportPosition.x is > 0f and < 1f &&
                             viewportPosition.y is > 0f and < 1f;
             return onScreen;
+        }
+
+
+        public object Save()
+        {
+            return new SaveData
+            {
+                Guids = _guids.ToArray(),
+            };
+        }
+
+
+        public void Load(object data)
+        {
+            SaveData saveData = (SaveData) data;
+
+            _guids = saveData.Guids.ToList();
+        }
+        
+        
+        private struct SaveData
+        {
+            public string[] Guids { get; set; }
         }
     }
 }
