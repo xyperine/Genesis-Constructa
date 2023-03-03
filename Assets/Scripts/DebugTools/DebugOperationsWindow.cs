a425e1091448a1b2cb4f20ae8c4d57ad0bd18c2c
ewsannne <ewsannne@gmail.com>
2023-03-03 23:42:30 +0400

    Added in-game debug ui

---
+++ Assets/Scripts/DebugTools/DebugOperationsWindow.cs
@@ -0,0 +1,37 @@
+﻿using ColonizationMobileGame.Structures;
+using UnityEngine;
+
+namespace ColonizationMobileGame.DebugTools
+{
+    public class DebugOperationsWindow : MonoBehaviour
+    {
+        [SerializeField] private DebugOperations debugOperations;
+
+        private StructureType _targetStructure;
+
+
+        public void SetTarget(int targetInt)
+        {
+            StructureType targetStructure = (StructureType) targetInt;
+            _targetStructure = targetStructure;
+        }
+
+
+        public void Unlock()
+        {
+            debugOperations.Unlock(_targetStructure);
+        }
+
+
+        public void UnlockAndBuild()
+        {
+            debugOperations.UnlockAndBuild(_targetStructure);
+        }
+
+
+        public void Upgrade()
+        {
+            debugOperations.Upgrade(_targetStructure);
+        }
+    }
+}
