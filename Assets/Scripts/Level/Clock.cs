29b53c3dea0dd3e48877441734e65339e0aecabb
ewsannne <ewsannne@gmail.com>
2023-03-05 01:41:13 +0400

    Added clock, probably temporary

---
+++ Assets/Scripts/Level/Clock.cs
@@ -0,0 +1,61 @@
+﻿using ColonizationMobileGame.SaveLoadSystem;
+using UnityEngine;
+
+namespace ColonizationMobileGame.Level
+{
+    public class Clock : MonoBehaviour, ISceneSaveable
+    {
+        [SerializeField, HideInInspector] private PermanentGuid guid;
+        
+        private bool _active;
+
+        public PermanentGuid Guid => guid;
+        public int LoadingOrder => 10;
+
+        public float ElapsedTimeInSeconds { get; private set; }
+
+
+        private void Start()
+        {
+            Activate();
+        }
+
+
+        public void Activate()
+        {
+            _active = true;
+        }
+
+
+        private void Update()
+        {
+            if (_active)
+            {
+                ElapsedTimeInSeconds += Time.deltaTime;
+            }
+        }
+
+
+        public object Save()
+        {
+            return new SaveData
+            {
+                ElapsedTimeInSeconds = ElapsedTimeInSeconds,
+            };
+        }
+
+
+        public void Load(object data)
+        {
+            SaveData saveData = (SaveData) data;
+
+            ElapsedTimeInSeconds = saveData.ElapsedTimeInSeconds;
+        }
+        
+        
+        private struct SaveData
+        {
+            public float ElapsedTimeInSeconds { get; set; }
+        }
+    }
+}
---
+++ Assets/Scripts/Level/ClockDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace ColonizationMobileGame.Level
+{
+    public class ClockDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro text;
+        [SerializeField] private Clock clock;
+
+
+        private void LateUpdate()
+        {
+            text.text = TimeSpan.FromSeconds(clock.ElapsedTimeInSeconds).ToString(@"hh\:mm\:ss");
+        }
+    }
+}
