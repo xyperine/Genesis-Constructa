76e7fc16e0ec15622111946d8ea6e68d192ff414
ewsannne <ewsannne@gmail.com>
2023-03-05 04:53:47 +0400

    Turned clock into timer

---
+++ Assets/Scripts/Timer/GameTimer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using ColonizationMobileGame.SaveLoadSystem;
+using UnityEngine;
+
+namespace ColonizationMobileGame.Timer
+{
+    public class GameTimer : MonoBehaviour, ISceneSaveable
+    {
+        [SerializeField, Range(10, 30)] private int minutes = 20;
+        
+        [SerializeField, HideInInspector] private PermanentGuid guid;
+        
+        private bool _active;
+
+        public PermanentGuid Guid => guid;
+        public int LoadingOrder => 10;
+
+        public float SecondsLeft { get; private set; }
+
+
+        private void Start()
+        {
+            SecondsLeft += minutes * 60;
+            
+            Activate();
+        }
+
+
+        public void Activate()
+        {
+            _active = true;
+        }
+
+
+        private void Update()
+        {
+            if (!_active)
+            {
+                return;
+            }
+
+            SecondsLeft -= Time.deltaTime;
+
+            if (!(SecondsLeft <= 0f))
+            {
+                return;
+            }
+
+            Deactivate();
+
+            Time.timeScale = 0f;
+            Debug.LogWarning("The time is out!");
+        }
+
+
+        public void Deactivate()
+        {
+            _active = false;
+        }
+
+
+        private void OnDisable()
+        {
+            Debug.LogWarning($"Time left: {TimeSpan.FromSeconds(SecondsLeft):mm\\:ss}");
+        }
+
+
+        public object Save()
+        {
+            return new SaveData
+            {
+                ElapsedTimeInSeconds = SecondsLeft,
+            };
+        }
+
+
+        public void Load(object data)
+        {
+            SaveData saveData = (SaveData) data;
+
+            SecondsLeft = saveData.ElapsedTimeInSeconds;
+        }
+        
+        
+        private struct SaveData
+        {
+            public float ElapsedTimeInSeconds { get; set; }
+        }
+    }
+}
---
+++ Assets/Scripts/Timer/GameTimerDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace ColonizationMobileGame.Timer
+{
+    public class GameTimerDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro text;
+        [SerializeField] private GameTimer gameTimer;
+
+
+        private void LateUpdate()
+        {
+            text.text = TimeSpan.FromSeconds(gameTimer.SecondsLeft).ToString(@"mm\:ss\:fff");
+        }
+    }
+}
