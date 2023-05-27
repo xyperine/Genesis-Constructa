5c3828c4ce0efc930d48087afbbae0d569a20884
ewsannne <ewsannne@gmail.com>
2023-05-27 04:09:36 +0400

    Added delay between purchases

--- Assets/Scripts/ItemsPlacementsInteractions/ItemsConsumer.cs
+++ Assets/Scripts/ItemsPlacementsInteractions/ItemsConsumer.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections;
 using ColonizationMobileGame.Items;
 using ColonizationMobileGame.ItemsPlacement.Core;
 using ColonizationMobileGame.ItemsPlacement.Movers;
 using ColonizationMobileGame.ItemsPlacementsInteractions.Target;
 using ColonizationMobileGame.ItemsRequirementsSystem;
+using ColonizationMobileGame.Utility.Helpers;
+using UnityEngine;
 
 namespace ColonizationMobileGame.ItemsPlacementsInteractions
 {
     public class ItemsConsumer : InteractionTarget
     {
-        private ItemsRequirementsChain _requirementsChain;
+        [SerializeField, Min(0f)] private float delayBetweenPurchasesInSeconds = 0.5f;
         
         private readonly DestroyingPlacementItemsMover _itemsMover = new DestroyingPlacementItemsMover();
+        
+        private ItemsRequirementsChain _requirementsChain;
+        private bool _onCooldown;
 
-        public override bool CanTakeMore => _requirementsChain.NeedMore;
+        public override bool CanTakeMore => !_onCooldown && _requirementsChain.NeedMore;
         public override ItemType[] AcceptableItems => _requirementsChain.RequiredItems;
 
         public event Action Consumed;
@@ -28,6 +34,13 @@ namespace ColonizationMobileGame.ItemsPlacementsInteractions
         public void Setup(ItemsRequirementsChain requirementsChain)
         {
             _requirementsChain = requirementsChain;
+            _requirementsChain.ChangingBlock += BeginWaitingForDelay;
+        }
+
+
+        private void Start()
+        {
+            BeginWaitingForDelay();
         }
 
 
@@ -39,6 +52,31 @@ namespace ColonizationMobileGame.ItemsPlacementsInteractions
             _itemsMover.MoveItem(item.GetComponent<PlacementItem>(), transform.position);
             
             Consumed?.Invoke();
+        }
+
+
+        private void BeginWaitingForDelay()
+        {
+            StartCoroutine(WaitForDelayRoutine());
+        }
+
+
+        private IEnumerator WaitForDelayRoutine()
+        {
+            _onCooldown = true;
+            
+            yield return YieldInstructionsHelpers.GetWaitForSeconds(delayBetweenPurchasesInSeconds);
+
+            _onCooldown = false;
+        }
+
+
+        private void OnDestroy()
+        {
+            if (_requirementsChain != null)
+            {
+                _requirementsChain.ChangingBlock -= BeginWaitingForDelay;
+            }
         }
     }
 }
